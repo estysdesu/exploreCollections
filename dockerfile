1b4e557bb75466348917a47dd26e3adfce28cd9e
Tyler Estes <estysdesu@gmail.com>
2020-02-11 16:37:58 -0500

    ABCs and finish ChainMap

--- dockerfile
+++ dockerfile
@@ -1,5 +1,5 @@
 FROM jupyter/minimal-notebook:latest
 WORKDIR $HOME/work
 # ENV JUPYTER_ENABLE_LAB yes
-# RUN python -m pip install bokeh
+RUN python -m pip install guppy3
 
--- entry.sh
+++ entry.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env sh
 
 PROJECT='collections'
-HASH='sha1:317cfe301aae:afd6cb95fdc6b48254a4fadf2419a1f4ffd93851' # collections
+PORT=8888
+HASH=`python -c "import IPython; print(IPython.lib.passwd('${PROJECT}'))"` # IPython (dependancy of Jupyter) must be install globally
 
-# Pull docker image before sending browser open to background
+# Pull/Build Docker image 
 # docker pull jupyter/minimal-notebook
 docker build . -t "${PROJECT}"
-sleep 5 && open "http://127.0.0.1:8888" &
 
-#docker run --rm -it -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan/work jupyter/minimal-notebook 
-#docker run --name "${PROJECT}" --rm -it -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v "${PWD}":/home/jovyan/work "${PROJECT}" start-notebook.sh --NotebookApp.password="${PASSWD}" # (https://jupyter-docker-stacks.readthedocs.io/en/latest/using/common.html#notebook-options)
-#docker run --name "${PROJECT}" --rm -it -p 8888:8888 -v "${PWD}":/home/jovyan/work "${PROJECT}" start-notebook.sh --NotebookApp.password="${HASH}" # https://jupyter-docker-stacks.readthedocs.io/en/latest/using/common.html#notebook-options
-docker run --name "${PROJECT}" --rm -it -p 8888:8888 -v "${PWD}":/home/jovyan/work "${PROJECT}" start.sh jupyter lab --LabApp.token='' # https://jupyter-docker-stacks.readthedocs.io/en/latest/using/recipes.html
+# Sleep for server to start up then open browser to URL (sent to background)
+#sleep 5 && python -m webbrowser -n "http://127.0.0.1:8888" &
+sleep 5 && open "http://127.0.0.1:${PORT}" &
+
+# Run Docker image
+#docker run --rm -it --name "${PROJECT}" -p "${PORT}":"${PORT}" -e JUPYTER_ENABLE_LAB=yes -v "${PWD}":/home/jovyan/work "${PROJECT}" start-notebook.sh --NotebookApp.password="${HASH}" # https://jupyter-docker-stacks.readthedocs.io/en/latest/using/common.html#notebook-options
+docker run --rm -it --name "${PROJECT}" -p "${PORT}":"${PORT}" -v "${PWD}":/home/jovyan/work "${PROJECT}" start.sh jupyter lab --LabApp.token='' # https://jupyter-docker-stacks.readthedocs.io/en/latest/using/recipes.html
